213bcc0df54263944ead086d091ee32d708126a1
EvgeniyMeteliza <81425065+EvgeniyMeteliza@users.noreply.github.com>
2021-06-25 17:27:51 +0300

    Update documentation, delete unneccessary consts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pytia',
-    version='0.3.4',
+    version='0.3.5',
     description='Package provides poller for Group-IB Threat Intelligence & Attribution product',
     python_requires='>=3.6.*',
     install_requires=['requests>=2.25.1'],
