9bd9d7e801135c52009b7b0d323bcd61a1dad30c
EvgeniyMeteliza <e.metelitsa@group-ib.com>
2021-08-20 15:05:51 +0300

    Setup update

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     description='Package provides poller for Group-IB Threat Intelligence & Attribution product',
     python_requires='>=3.6.*',
     install_requires=['requests>=2.25.1'],
-    packages=['pytia']
+    packages=['pytia'],
+    author='Evgeniy Meteliza',
+    author_email='e.metelitsa@group-ib.com',
+    license='MIT',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
 )
